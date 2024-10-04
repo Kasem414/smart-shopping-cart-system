0b83048948821ec9f4d005939e15039875d18253
Kasem414 <kasm0945@gmail.com>
2024-10-04 09:29:41 +0300

    simplejwt configuration

---
+++ backend/project/myapp/api/views.py
@@ -0,0 +1,28 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls,user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['email'] = user.email
+        token['first_name'] = user.first_name
+        token['last_name'] = user.last_name
+        token['type'] = user.type
+        return token
+
+#Custom View Class
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class=MyTokenObtainPairSerializer
+    
+@api_view(['GET'])
+def getRoutes(request):
+    routes=[
+        'api/token',
+        'api/token/refresh'
+    ]
+    return Response(routes)
