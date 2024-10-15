958179265db7e22d26b92f92d69614beb0803df0
Kasem414 <kasm0945@gmail.com>
2024-10-15 07:59:50 +0300

    custom admin site and user admin to slove issue related login to admin dashboard

--- backend/project/myapp/admin.py
+++ backend/project/myapp/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
 from .models import MyUser
+from django.contrib.admin import AdminSite
+class MyAdminSite(AdminSite):
+    def has_permission(self, request):
+        return True
+    def has_module_perms(self,app_label):
+        return True
+    def has_perm(sef,perm,obj=None):
+        return True
+admin_site = MyAdminSite(name='myadmin')
+class MyUserAdmin(admin.ModelAdmin):
+    def has_view_permission(self, request, obj = ...):
+        return True
+    def has_add_permission(self, request):
+        return True
+    def has_change_permission(self, request, obj = ...):
+        return True
+    def has_module_permission(self, request):
+        return True
+admin_site.register(MyUser,MyUserAdmin)
 
-admin.site.register(MyUser)
+
--- backend/project/project/urls.py
+++ backend/project/project/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from myapp.admin import admin_site
 from django.urls import path,include,re_path
 from django.conf.urls.static import static
 from . import settings
@@ -8,7 +9,7 @@ from drf_yasg import openapi
 schema_view = get_schema_view(openapi.Info(title="Smart Shopping Cart API",default_version='v1',description="API Documentation"),public=True,permission_classes=[permissions.AllowAny])
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/',admin_site.urls),
     path('', include('myapp.urls')),
     path('api/',include('myapp.api.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$',schema_view.without_ui(cache_timeout=0),name='schema-swagger-ui'),
