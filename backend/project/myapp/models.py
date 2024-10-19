66f0e9590b3ae0dd29561d4b8a6df9d5dc83bb78
Kasem414 <kasm0945@gmail.com>
2024-10-19 12:14:27 +0300

    adding product and category models

--- backend/project/myapp/models.py
+++ backend/project/myapp/models.py
@@ -65,4 +65,39 @@ class MyUser(AbstractBaseUser,PermissionsMixin):
         return self.is_superuser
     def has_module_perms(self, app_label):
         return self.is_superuser
-    
+
+class Category(models.Model):
+    name = models.CharField(max_length=200,unique=True)
+    slug = models.SlugField(max_length=200,unique=True)
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+            ]
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
+    name = models.CharField(max_length=200,unique=True)
+    slug = models.CharField(max_length=200,unique=True)
+    description = models.TextField(blank=True,null=True)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    quantity = models.IntegerField()
+    image = models.ImageField(upload_to='media/%y/%m/%d',blank=True)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        indexes = [
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+            ]
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.name
